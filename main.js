fa19760698851ea15879f5cc15c5b4214a2bf877
Jeffrey Tom <Jeffrey.tom90@gmail.com>
2015-10-07 21:47:21 -0400

    added alerts

---
+++ main.js
@@ -0,0 +1,18 @@
+var myName = "Jeff";
+var herName = "Jessi";
+
+console.log (myName + herName)
+
+// var x = 7;
+// var y = 10;
+
+// console.log(x + y)
+
+alert("Good Evening, Mr. Bond");
+
+alert("Hello, How are you today?");
+
+String - "You've won the lottery! Hooray!"
+
+Number - 1,2,3,4,-2,-5
+
