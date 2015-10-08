07af2fdb85bb4ad05ae285bb976bd274dc62db91
Jeffrey Tom <Jeffrey.tom90@gmail.com>
2015-10-07 22:20:51 -0400

    added arrays

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
-var myName = "Jeff";
-var herName = "Jessi";
+// var myName = "Jeff";
+// var herName = "Jessi";
 
-console.log (myName + herName)
+// console.log (myName + herName)
 
-// var x = 7;
-// var y = 10;
+// // var x = 7;
+// // var y = 10;
 
-// console.log(x + y)
+// // console.log(x + y)
 
-alert("Good Evening, Mr. Bond");
+// alert("Good Evening, Mr. Bond");
 
-alert("Hello, How are you today?");
+// alert("Hello, How are you today?");
 
-String - "You've won the lottery! Hooray!"
+// String - "You've won the lottery! Hooray!"
 
-Number - 1,2,3,4,-2,-5
+// Number - 1,2,3,4,-2,-5
 
+// if (10 > 7) {
+// 		console.log("true");
+// 	} else {
+// 		console.log("false");
+
+var myluckyNumbers = [17,23,45,20,3,48];
+var myfavoriteCars = ["mercedes", "lexus", "infiniti"];
+
+console.log (myluckyNumbers, myfavoriteCars);
